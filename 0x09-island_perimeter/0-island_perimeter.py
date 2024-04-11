9c7e9ebbac8c76f7f7de1b8fe25c56052cc7704c
Aishah <maishah2540@gmail.com>
2024-04-11 12:52:23 +0300

    Wrote island_perimeter function

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+Calculates the perimeter of a grid-based island.
+'''
+
+
+def island_perimeter(grid):
+    '''Returns the perimeter of an island.
+
+    Args:
+    grid (list): A 2D matrix representing the grid containing the island
+                 whose perimeter is to be calculated.
+
+    Returns: the perimeter of the island in grid.
+    '''
+    # If the grid is empty, there is no island, perimeter = 0.
+    if not grid:
+        return 0
+
+    # Get the width and height of the grid.
+    width, height = len(grid), len(grid[0])
+    # Start perimeter at 0.
+    perimeter = 0
+    # Traverse through the grid index by index.
+    for i in range(width):
+        for j in range(height):
+            # If there is land, add the number of 0s around the land to perimeter.
+            if grid[i][j] == 1:
+                perimeter = perimeter + int(not grid[i][j - 1]) + int(
+                    not grid[i][j + 1]) + int(not grid[i - 1][j]) + int(not grid[i + 1][j])
+    return perimeter
