4668ad8f759f7c3244e4bc910f02de88da3e39a9
Aishah <maishah2540@gmail.com>
2024-04-18 19:54:43 +0000

    Fixed pycodestyle error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,7 +7,7 @@ Prime Game module.
 def isWinner(x, nums):
     """
     Determines the winner of a prime game session.
-    
+
     Args:
         x (int): The number of rounds.
         nums (list): Array of max limits.
@@ -32,15 +32,15 @@ def isWinner(x, nums):
             continue
         for j in range(i + i, n + 1, i):
             primes[j - 1] = False
-    
+
     # Filter the number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
         marias_wins += primes_count % 2 == 1
-    
+
     # Return None if they both have the same number of wins.
     if marias_wins == bens_wins:
         return None
-    
+
     return 'Maria' if marias_wins > bens_wins else 'Ben'
